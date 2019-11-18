51f210c4ca99b576e79cfc2859502a7b8bf29d37
migamcon <57899065+migamcon@users.noreply.github.com>
2019-11-18 13:04:03 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -19,7 +19,10 @@ public class Greeter {
   public String greet(String someone) {
     Long lalala = new Long("2333");
     if(MAGICO > 1){
+      if(MAGICO > 1){      
         return String.format("Hello, %s!", someone);
+      }
+    }
     }else{
         return String.format("Hello, %s!", someone);
     }
